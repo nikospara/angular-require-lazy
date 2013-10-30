12c677a67511bd475992a97bab22167ad250177a
nikospara <nikospara@yahoo.com>
2013-10-30 22:38:17 +0200

    Simplified command-line Less generation

--- build-scripts/bootstrap.bat
+++ build-scripts/bootstrap.bat
@@ -1,15 +1,7 @@
 @echo off
 mkdir ..\build\css\css
 
-REM node lessc --yui-compress ..\WebContent\css\style.less ..\build\css\style.css
-REM node lessc --yui-compress ..\WebContent\css\overrides.less ..\build\css\overrides.css
-REM node lessc --yui-compress ..\WebContent\css\bootstrap\bootstrap.less ..\build\css\bootstrap\bootstrap.css
-REM node lessc --yui-compress ..\WebContent\css\bootstrap\responsive.less ..\build\css\bootstrap\responsive.css
-
-node lessc --yui-compress ..\WebContent\css\bootstrap\bootstrap.less ..\build\css\css\style.css
-node lessc --yui-compress ..\WebContent\css\custom\style.less >> ..\build\css\css\style.css
-node lessc --yui-compress ..\WebContent\css\bootstrap\responsive.less >> ..\build\css\css\style.css
-node lessc --yui-compress ..\WebContent\css\custom\overrides.less >> ..\build\css\css\style.css
+node lessc --yui-compress ..\WebContent\css\style.less ..\build\css\css\style.css
 
 mkdir ..\build\css\images
 copy /Y ..\WebContent\css\images ..\build\css\images
--- build-scripts/bootstrap.sh
+++ build-scripts/bootstrap.sh
@@ -2,10 +2,7 @@
 
 if [ ! -d ../build/css/css ]; then mkdir -p ../build/css/css; fi
 
-node lessc --yui-compress ../WebContent/css/bootstrap/bootstrap.less ../build/css/css/style.css
-node lessc --yui-compress ../WebContent/css/custom/style.less >> ../build/css/css/style.css
-node lessc --yui-compress ../WebContent/css/bootstrap/responsive.less >> ../build/css/css/style.css
-node lessc --yui-compress ../WebContent/css/custom/overrides.less >> ../build/css/css/style.css
+node lessc --yui-compress ../WebContent/css/style.less ../build/css/css/style.css
 
 if [ ! -d ../build/css/images ]; then mkdir -p ../build/css/images; fi
 cp -f ../WebContent/css/images/* ../build/css/images
